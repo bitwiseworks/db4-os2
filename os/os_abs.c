93c7cc221c35786689ae07a0f8d0d7cba002ebb8
Yuri Dario <yd@os2power.com>
2011-01-01 17:23:22 +0000

    bdb: applied os2 patches, enabled tcl, build as dll.
    git-svn-id: http://svn.netlabs.org/repos/ports/bdb/trunk@258 8d6eb4b1-0faa-de11-b929-00e081727c95

--- os/os_abs.c
+++ os/os_abs.c
@@ -20,5 +20,15 @@ int
 __os_abspath(path)
 	const char *path;
 {
-	return (path[0] == '/');
+#ifdef __EMX__
+	/*
+	 * !!!
+	 * Check for drive specifications, e.g., "C:".  In addition, the path
+	 * separator used by the win32 DB (PATH_SEPARATOR) is \; look for both
+	 * / and \ since these are user-input paths.
+	 */
+	if (isalpha(path[0]) && path[1] == ':')
+		path += 2;
+#endif
+	return (path[0] == '/' || path[0] == '\\');
 }
